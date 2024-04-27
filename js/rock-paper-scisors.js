7ee5322aceeebfda8dc1099f6efa63b21466b516
DOOMNOTRON <edgarcruztacoma@gmail.com>
2024-04-27 00:37:08 -0700

    Added playgame function, It retrieves the player selection and generate the Ai selection from a string array.

--- js/rock-paper-scisors.js
+++ js/rock-paper-scisors.js
@@ -13,19 +13,35 @@ var drawCount = 0;
 // Simulating a game scenario
 // Let's say the user clicks a button to play the game, and based on some logic, they either win, lose, or draw
 // code not implemented or working yet
-function playGame() {
-    // Simulated game logic
+function playGame(playerMove) {
+    /*Simulated game logic
     var randomNumber = Math.random(); // Generate a random number between 0 and 1
 
-    // If the random number is greater than 0.5, consider it a win, otherwise a loss
+    If the random number is greater than 0.5, consider it a win, otherwise a loss
     if (randomNumber > 0.5) {
         winCount++;
     } else {
         loseCount++;
     }
+    */
+   console.log('Player selects: ' + playerMove);
+   var aiMove = aiMoves();
+
+
+
+
 
     // Update the score display
     document.getElementById("winCount").textContent = winCount;
     document.getElementById("loseCount").textContent = loseCount;
     document.getElementById("drawCount").textContent = drawCount;
+    return;
+}
+
+function aiMoves() {
+    var gameArray = ['rock', 'paper', 'scissors'];
+    var randomSelection = gameArray[Math.floor(Math.random() * gameArray.length)];
+
+    console.log('Ai selects: ' + randomSelection);
+    return randomSelection;
 }
